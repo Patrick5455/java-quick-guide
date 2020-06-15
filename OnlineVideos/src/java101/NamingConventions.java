bb1aa4298aeedd2ec3e5827ee339e0915579d1cc
Patrick5455 <patrickojunde@gmail.com>
2020-06-15 10:01:50 +0100

    Create Class Operators in java > Types of operators in java > Practice Arithmetric operators in java > Practice post & pre increment/decrement in java

--- OnlineVideos/src/java101/NamingConventions.java
+++ OnlineVideos/src/java101/NamingConventions.java
@@ -9,14 +9,33 @@ public class NamingConventions {
 		
 		/* The Following are what can be named in java:
 		 * 
-		 * -Package
-		 * -Interface
-		 * -Class
-		 * -Methods
-		 * -Variables
-		 * -Constants
-		 * -
+		 * -Package : this should be all lower case
+		 * -Interface : this should start with Capital Letter
+		 * -Class: this should start with Capital letter
+		 * -Methods: this should be in lower case
+		 * -Variables: this should be in lower case
+		 * -Constants: this should be all CAPITAL LETTERS
+		 * - 
 		 * */
+		
+		
+		// Camel Case Rule in Java:
+		
+		/* when combining two words or more as names in java
+		 * you join them together with the next word starting with a capital letter
+		 * e.g: 
+		 * 		NewClass
+		 * 		newMethod
+		 * 		newPackage
+		 * 		NewInterface
+		 * 
+		 * However, this does not apply to constant or ENUM  types
+		 * since they are written in UPPERCASE all through, we are separated by underscores
+		 * e.g THIS_IS_ENUM 
+		 * 
+		 * */
+		
+		// It is important to 
 		
 	}
 
